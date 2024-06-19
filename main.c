713a025714e58361b871ebf7e56c723478ade479
m <49080343+opp@users.noreply.github.com>
2024-06-19 19:42:11 -0400

    Add pp gen

--- main.c
+++ main.c
@@ -3,50 +3,71 @@
 #include <stdlib.h>
 #include <time.h>
 
-void gen(int gen_len)
+int gen(char **argv)
 {
+    int gen_len;
+    if (argv[2] == NULL) {
+        gen_len = 12;
+    } else {
+        gen_len = atoi(argv[2]);
+    }
+
     char gen[gen_len];
-    int randomizer = 0;
     srand((unsigned int)(time(NULL)));
 
-    char *numbers = "0123456789";
-    char *lower = "abcdefghijklmnoqprstuvwyzx";
     char *upper = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+    char *lower = "abcdefghijklmnoqprstuvwyzx";
+    char *numbers = "0123456789";
     char *special = "!@#$^&*?%_";
 
+    int randomizer = 0;
+    int randomizer_modval = 4;
+
+    if (strcmp(argv[1], "pw") == 0) {
+        if (argv[3] != NULL) {
+            randomizer_modval = 4;
+        } else {
+            randomizer_modval = 3;
+        }
+    } else if (strcmp(argv[1], "pp") == 0) {
+        if (argv[3] != NULL) {
+            randomizer_modval = 3;
+        } else {
+            randomizer_modval = 2;
+        }
+    } else {
+        return 1;
+    }
+
     for (int i = 0; i < gen_len; ++i) {
-        randomizer = rand() % 4;
-        if (randomizer == 1) {
-            gen[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
+        randomizer = rand() % randomizer_modval;
+        if (randomizer == 0) {
+            gen[i] = upper[rand() % 26];
+            printf("%c", gen[i]);
+        }
+        else if (randomizer == 1) {
+            gen[i] = lower[rand() % 26];
             printf("%c", gen[i]);
         }
         else if (randomizer == 2) {
-            gen[i] = special[rand() % 8];
-            randomizer = rand() % 4;
-            printf("%c", gen[i]);
-        }
-        else if (randomizer == 3) {
-            gen[i] = upper[rand() % 26];
-            randomizer = rand() % 4;
+            gen[i] = numbers[rand() % 10];
             printf("%c", gen[i]);
         }
         else {
-            gen[i] = lower[rand() % 26];
-            randomizer = rand() % 4;
+            gen[i] = special[rand() % 10];
             printf("%c", gen[i]);
         }
     }
     printf("\n");
+    return 0;
 }
 
+// pgwen [gen_type *required] [gen_length] [include_special|include_numbers]
 int main(int argc, char **argv)
 {
     if (argc == 1) {
         return 1;
+    } else {
+        gen(argv);
     }
-    if (strcmp(argv[1], "pw") == 0 && argv[2] != NULL) {
-        gen(atoi(argv[2]));
-    }
-    return 0;
 }
